e0921281e2350e886f1d179b66146e22246a6f83
Rudolf Hu <zuirenlangzi@gmail.com>
2025-06-05 03:38:32 -0500

    6/5/2025

--- computational_modeling.py
+++ computational_modeling.py
@@ -39,6 +39,8 @@ if __name__ == '__main__':
     model_decay_win = ComputationalModels(model_type='decay_win', condition='Gains')
     model_dual = DualProcessModel()
 
+    model_list = [model_delta, model_delta_PVL, model_decay, model_decay_win, model_dual]
+
     n_iterations = 100
 
     # # test the data
@@ -53,7 +55,7 @@ if __name__ == '__main__':
     # Fit all data (Since E1 is between-subjects, we can fit all data together)
     model_names = ['delta', 'delta_PVL', 'decay', 'decay_win', 'dual']
 
-    for i, model in enumerate([model_delta, model_delta_PVL, model_decay, model_decay_win, model_dual]):
+    for i, model in enumerate([model_list]):
             save_dir = f'./data/ModelFitting/E1/{model_names[i]}_results.csv'
             # Check if the file already exists
             try:
@@ -101,32 +103,54 @@ if __name__ == '__main__':
     #             model_results = model.fit(E2_dict, num_training_trials=120, num_iterations=n_iterations)
     #
     #         model_results.to_csv(save_dir, index=False)
-    # # ------------------------------------------------------------------------------------------------------------------
-    # # for baseline data
-    # # ------------------------------------------------------------------------------------------------------------------
-    # delta_baseline = model_delta.fit(baseline_dict, num_iterations=200)
-    # delta_baseline.to_csv('./data/ModelFitting/delta_baseline_id.csv', index=False)
-    #
-    # decay_baseline = model_decay.fit(baseline_dict, num_iterations=200)
-    # decay_baseline.to_csv('./data/ModelFitting/decay_baseline_id.csv', index=False)
-    #
-    # dual_baseline = model_dual.fit(baseline_dict, 'Entropy_Dis_ID', Gau_fun='Naive_Recency',
-    #                                   Dir_fun='Linear_Recency', weight_Dir='softmax', weight_Gau='softmax',
-    #                                   num_iterations=200)
-    # dual_baseline.to_csv('./data/ModelFitting/dual_baseline_id.csv', index=False)
+    # ------------------------------------------------------------------------------------------------------------------
+    # for baseline data
+    # ------------------------------------------------------------------------------------------------------------------
+    for i, model in enumerate(model_list):
+            save_dir = f'./data/ModelFitting/E2/Baseline/{model_names[i]}_results.csv'
+            # Check if the file already exists
+            try:
+             existing_results = pd.read_csv(save_dir)
+             if not existing_results.empty:
+                  print(f"File {save_dir} already exists. Skipping model fitting.")
+                  continue
+            except FileNotFoundError:
+             pass
+
+            if model_names[i] == 'dual':
+                # Fit the dual-process model
+                model_results = model.fit(E2_baseline_dict, 'Dual_Process', Gau_fun='Naive_Recency', Dir_fun='Linear_Recency',
+                                          weight_Dir='softmax', weight_Gau='softmax', num_training_trials=120,
+                                          num_iterations=n_iterations)
+            else:
+            # For other models, fit them directly
+                model_results = model.fit(E2_baseline_dict, num_training_trials=120, num_iterations=n_iterations)
+
+            model_results.to_csv(save_dir, index=False)
 
     # ------------------------------------------------------------------------------------------------------------------
     # for frequency data
     # ------------------------------------------------------------------------------------------------------------------
-    # delta_frequency = model_delta.fit(frequency_dict, num_iterations=200)
-    # delta_frequency.to_csv('./data/ModelFitting/delta_frequency_id.csv', index=False)
-    #
-    # decay_frequency = model_decay.fit(frequency_dict, num_iterations=200)
-    # decay_frequency.to_csv('./data/ModelFitting/decay_frequency_id.csv', index=False)
+    for i, model in enumerate(model_list):
+            save_dir = f'./data/ModelFitting/E2/Frequency/{model_names[i]}_results.csv'
+            # Check if the file already exists
+            try:
+             existing_results = pd.read_csv(save_dir)
+             if not existing_results.empty:
+                  print(f"File {save_dir} already exists. Skipping model fitting.")
+                  continue
+            except FileNotFoundError:
+             pass
 
-    # dual_frequency = model_dual.fit(frequency_dict, 'Entropy_Dis_ID', Gau_fun='Naive_Recency',
-    #                                     Dir_fun='Linear_Recency', weight_Dir='softmax', weight_Gau='softmax',
-    #                                     num_iterations=200)
-    # dual_frequency.to_csv('./data/ModelFitting/dual_frequency_id.csv', index=False)
+            if model_names[i] == 'dual':
+                # Fit the dual-process model
+                model_results = model.fit(E2_frequency_dict, 'Dual_Process', Gau_fun='Naive_Recency', Dir_fun='Linear_Recency',
+                                          weight_Dir='softmax', weight_Gau='softmax', num_training_trials=120,
+                                          num_iterations=n_iterations)
+            else:
+            # For other models, fit them directly
+                model_results = model.fit(E2_frequency_dict, num_training_trials=120, num_iterations=n_iterations)
+
+            model_results.to_csv(save_dir, index=False)
 
 
